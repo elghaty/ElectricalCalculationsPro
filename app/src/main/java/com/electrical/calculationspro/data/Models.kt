998c1b1428dc26b16b9219d17de95224cf63c5da
elghaty <elghaty@gmail.com>
2026-09-12 10:59:24 +0300

    Add data classes and enums for electrical calculations

--- app/src/main/java/com/electrical/calculationspro/data/Models.kt
+++ app/src/main/java/com/electrical/calculationspro/data/Models.kt
@@ -1,1 +1,51 @@
+package com.electrical.calculationspro.data
 
+enum class Standard { IEC, EGYPTIAN, CEI, NEC, CEC }
+
+enum class CurrentType {
+    DirectCurrent,
+    AlternatingSinglePhase,
+    AlternatingTwoPhase,
+    AlternatingThreePhase
+}
+
+enum class ConductorMaterial { Copper, Aluminum }
+
+enum class InsulationType { PVC, XLPE, EPR, Rubber }
+
+data class InstallationMethod(
+    val code: String,
+    val description: String
+)
+
+val iecInstallationMethods = IecTables.allInstallationMethods
+
+data class ConductorSizingInput(
+    val currentType: CurrentType = CurrentType.AlternatingSinglePhase,
+    val voltage: Double = 230.0,
+    val load: Double = 5000.0,
+    val powerFactor: Double = 0.9,
+    val lineLength: Double = 60.0,
+    val installationMethod: InstallationMethod = iecInstallationMethods[0],
+    val ambientTemp: Double = 30.0,
+    val conductor: ConductorMaterial = ConductorMaterial.Copper,
+    val insulation: InsulationType = InsulationType.PVC,
+    val circuitsInConduit: Int = 1,
+    val maxVoltageDrop: Double = 4.0
+)
+
+data class ConductorSizingResult(
+    val designCurrent: Double,
+    val recommendedSection: Double,
+    val selectedSection: Double,
+    val ampacity: Double,
+    val voltageDropPercent: Double,
+    val voltageDropVolts: Double,
+    val protectiveDevice: Double,
+    val notes: List<String>
+)
+
+val standardSections = listOf(
+    1.5, 2.5, 4.0, 6.0, 10.0, 16.0, 25.0, 35.0, 50.0,
+    70.0, 95.0, 120.0, 150.0, 185.0, 240.0, 300.0, 400.0
+)
