71b2143d8a1f9023d1e8660c9576d3bb0f1cf566
elghaty <elghaty@gmail.com>
2026-09-12 10:55:49 +0300

    Add color definitions for dark theme and accents

--- app/src/main/java/com/electrical/calculationspro/ui/theme/Color.kt
+++ app/src/main/java/com/electrical/calculationspro/ui/theme/Color.kt
@@ -1,1 +1,18 @@
+package com.electrical.calculationspro.ui.theme
 
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF1A1F2E)
+val DarkSurface = Color(0xFF232937)
+val DarkSurfaceVariant = Color(0xFF2A3142)
+val PrimaryTeal = Color(0xFF00BCD4)
+val PrimaryTealDark = Color(0xFF0097A7)
+val TextPrimary = Color(0xFFE0E0E0)
+val TextSecondary = Color(0xFFB0B0B0)
+val AccentGreen = Color(0xFF4CAF50)
+val AccentRed = Color(0xFFE53935)
+val AccentOrange = Color(0xFFFF9800)
+val AccentPurple = Color(0xFF9C27B0)
+val AccentYellow = Color(0xFFFFEB3B)
+val SidebarSelected = Color(0xFF2C3548)
+val DividerColor = Color(0xFF3A4255)
