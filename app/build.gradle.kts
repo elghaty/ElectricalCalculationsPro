a7038c0377570ca0f7180ccbb4c1c22fdc2d3d55
elghaty <elghaty@gmail.com>
2026-09-12 12:09:32 +0300

    Refactor dependencies and build options in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,38 +19,84 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2024.10.01")
+
+    val composeBom =
+        platform(
+            "androidx.compose:compose-bom:2024.10.01"
+        )
+
     implementation(composeBom)
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.navigation:navigation-compose:2.8.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.compose.foundation:foundation")
-    
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation(
+        "androidx.compose.ui:ui"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui-graphics"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    implementation(
+        "androidx.compose.material:material-icons-extended"
+    )
+
+    implementation(
+        "androidx.compose.foundation:foundation"
+    )
+
+    implementation(
+        "androidx.activity:activity-compose:1.9.3"
+    )
+
+    implementation(
+        "androidx.navigation:navigation-compose:2.8.3"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-runtime-ktx:2.8.7"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-test-manifest"
+    )
 }
