792ec321bffb6c487de39406333ef17fa290ae65
elghaty <elghaty@gmail.com>
2026-09-12 12:25:16 +0300

    Update Models.kt

--- app/src/main/java/com/electrical/calculationspro/data/Models.kt
+++ app/src/main/java/com/electrical/calculationspro/data/Models.kt
@@ -1,6 +1,12 @@
 package com.electrical.calculationspro.data
 
-enum class Standard { IEC, EGYPTIAN, CEI, NEC, CEC }
+enum class Standard {
+    IEC,
+    EGYPTIAN,
+    CEI,
+    NEC,
+    CEC
+}
 
 enum class CurrentType {
     DirectCurrent,
@@ -9,9 +15,17 @@ enum class CurrentType {
     AlternatingThreePhase
 }
 
-enum class ConductorMaterial { Copper, Aluminum }
+enum class ConductorMaterial {
+    Copper,
+    Aluminum
+}
 
-enum class InsulationType { PVC, XLPE, EPR, Rubber }
+enum class InsulationType {
+    PVC,
+    XLPE,
+    EPR,
+    Rubber
+}
 
 data class InstallationMethod(
     val code: String,
@@ -24,9 +38,9 @@ data class ConductorSizingInput(
     val currentType: CurrentType = CurrentType.AlternatingSinglePhase,
     val voltage: Double = 230.0,
     val load: Double = 5000.0,
-    val powerFactor: Double = 0.9,
+    val powerFactor: Double = 0.90,
     val lineLength: Double = 60.0,
-    val installationMethod: InstallationMethod = iecInstallationMethods[0],
+    val installationMethod: InstallationMethod = iecInstallationMethods.first(),
     val ambientTemp: Double = 30.0,
     val conductor: ConductorMaterial = ConductorMaterial.Copper,
     val insulation: InsulationType = InsulationType.PVC,
@@ -42,10 +56,28 @@ data class ConductorSizingResult(
     val voltageDropPercent: Double,
     val voltageDropVolts: Double,
     val protectiveDevice: Double,
+    val shortCircuitCurrentKA: Double = 0.0,
+    val breakerWithinCableCapacity: Boolean = false,
+    val voltageDropWithinLimit: Boolean = false,
     val notes: List<String>
 )
 
 val standardSections = listOf(
-    1.5, 2.5, 4.0, 6.0, 10.0, 16.0, 25.0, 35.0, 50.0,
-    70.0, 95.0, 120.0, 150.0, 185.0, 240.0, 300.0, 400.0
+    1.5,
+    2.5,
+    4.0,
+    6.0,
+    10.0,
+    16.0,
+    25.0,
+    35.0,
+    50.0,
+    70.0,
+    95.0,
+    120.0,
+    150.0,
+    185.0,
+    240.0,
+    300.0,
+    400.0
 )
