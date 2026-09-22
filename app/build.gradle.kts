207c22fe8cf199645ddd90635c6afa89dc287b9f
elghaty <elghaty@gmail.com>
2026-09-22 12:10:03 +0300

    Refactor dependencies in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,6 @@ android {
         applicationId = "com.electrical.calculationspro"
 
         minSdk = 24
-
         targetSdk = 35
 
         versionCode =
@@ -65,43 +64,21 @@ android {
 
 dependencies {
 
-    implementation(
-        "androidx.core:core-ktx:1.15.0"
-    )
+    implementation("androidx.core:core-ktx:1.15.0")
 
-    implementation(
-        "androidx.activity:activity-compose:1.10.0"
-    )
+    implementation("androidx.activity:activity-compose:1.10.0")
 
-    implementation(
-        "androidx.compose.ui:ui:1.7.6"
-    )
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
 
-    implementation(
-        "androidx.compose.material3:material3:1.3.1"
-    )
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
 
-    implementation(
-        "androidx.compose.ui:ui-tooling-preview:1.7.6"
-    )
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
-    debugImplementation(
-        "androidx.compose.ui:ui-tooling:1.7.6"
-    )
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
-    implementation(
-        "androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7"
-    )
-
-    implementation(
-        "androidx.room:room-runtime:2.6.1"
-    )
-
-    implementation(
-        "androidx.room:room-ktx:2.6.1"
-    )
-
-    ksp(
-        "androidx.room:room-compiler:2.6.1"
-    )
+    ksp("androidx.room:room-compiler:2.6.1")
 }
